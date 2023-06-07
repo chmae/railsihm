e219afc1804e1b879de15766d3b53c6d2e554f3e
Asiamar Chaimae <chaimae.asiamar@etu.umontpellier.fr>
2023-06-07 19:31:07 +0000

    essayer_mais_ne_marche_pas

--- src/main/java/fr/umontpellier/iut/rails/vues/VueChoixJoueurs.java
+++ src/main/java/fr/umontpellier/iut/rails/vues/VueChoixJoueurs.java
@@ -1,9 +1,18 @@
 package fr.umontpellier.iut.rails.vues;
 
+import fr.umontpellier.iut.rails.IJoueur;
 import javafx.beans.value.ChangeListener;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
@@ -24,21 +33,26 @@ public class VueChoixJoueurs extends Stage {
 
     public VueChoixJoueurs() {
         nomsJoueurs = FXCollections.observableArrayList();
+        initAvatar();
     }
 
     public List<String> getNomsJoueurs() {
         return nomsJoueurs;
     }
 
+
     /**
      * Définit l'action à exécuter lorsque la liste des participants est correctement initialisée
      */
-    public void setNomsDesJoueursDefinisListener(ListChangeListener<String> quandLesNomsDesJoueursSontDefinis) {}
+    public void setNomsDesJoueursDefinisListener(ListChangeListener<String> quandLesNomsDesJoueursSontDefinis) {
+     //   nomsJoueurs.addListener(quandLesNomsDesJoueursSontDefinis);
+    }
 
     /**
      * Définit l'action à exécuter lorsque le nombre de participants change
      */
-    protected void setChangementDuNombreDeJoueursListener(ChangeListener<Integer> quandLeNombreDeJoueursChange) {}
+    protected void setChangementDuNombreDeJoueursListener(ChangeListener<Integer> quandLeNombreDeJoueursChange) {
+    }
 
     /**
      * Vérifie que tous les noms des participants sont renseignés
@@ -66,7 +80,7 @@ public class VueChoixJoueurs extends Stage {
      * Retourne le nombre de participants à la partie que l'utilisateur a renseigné
      */
     protected int getNombreDeJoueurs() {
-        throw new RuntimeException("Methode à implémenter");
+        return getNomsJoueurs().size();
     }
 
     /**
@@ -74,7 +88,39 @@ public class VueChoixJoueurs extends Stage {
      * @param playerNumber : le numéro du participant
      */
     protected String getJoueurParNumero(int playerNumber) {
-        throw new RuntimeException("Methode à implémenter");
+        List<String> nomsJoueurs = getNomsJoueurs();
+        if (playerNumber > 0 && playerNumber <= nomsJoueurs.size()) {
+            return nomsJoueurs.get(playerNumber - 1);
+        }
+        return null;
     }
+
+    private void initAvatar(){
+        //joueursAvatar.getChildren().clear();
+        ImageView img;
+
+        for (String j : nomsJoueurs) {
+
+            HBox p = new HBox();
+            VBox vb = new VBox();
+
+            img = new ImageView("images/cartesWagons/avatar-" + j + ".png");
+            img.setFitHeight(83);
+            img.setFitWidth(105);
+            vb.getChildren().add(img);
+
+            Label nom = new Label(j);
+            nom.prefWidthProperty().bind(img.fitWidthProperty());
+            nom.setAlignment(Pos.CENTER);
+            vb.getChildren().add(nom);
+
+            p.getChildren().add(vb);
+
+
+
+
+        }
+    }
+
 
 }
